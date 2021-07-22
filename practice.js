8d55616e4d7c6add68b9d3bab9970dd17d9c481e
Brian <b.itumo18@gmail.com>
2021-07-22 18:19:26 +0300

    first commit

---
+++ practice.js
@@ -0,0 +1,15 @@
+const person = {
+  fname: "brian",
+  lname: "Itumo",
+  gender: "male",
+  age: 30,
+  hobbies: ["cooking", "dancing", "laughing"],
+  address: {
+    city: "Mombasa",
+    address: 87127,
+    governor: "me",
+    mtaa: "Mikindani",
+  },
+};
+
+console.log(person.address.city,person.address.mtaa);
